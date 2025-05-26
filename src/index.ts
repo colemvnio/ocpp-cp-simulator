92b3bef0bb79b9d4227e720aee5e89070fc21377
Patrick Coleman <patrick.coleman.1@ulaval.ca>
2025-05-25 20:54:09 -0400

    feat(websocket): Establish WebSocketClient, Playground

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import WebSocketClient from './infrastructure/messaging/WebSocketClient';
+
+const client = new WebSocketClient('test-client', 'ws://localhost:8080', 'ocpp1.6');
+
+client.connect()
+  .then(() => {
+    console.log('Connected!');
+    client.send(JSON.stringify({ message: 'Hello OCPP server!' }));
+    client.ping();
+  })
+  .catch((err) => {
+    console.error('Connection failed:', err);
+  });
