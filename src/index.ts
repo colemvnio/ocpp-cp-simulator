493defb94ba70fa499b224fc5bdda03455fd2642
Patrick Coleman <patrick.coleman.1@ulaval.ca>
2025-05-25 22:00:14 -0400

    feat(scenario): Simplified manual scenario

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,42 @@ import { ChargePoint } from './domain/entities/chargepoint.entity';
 import { MessageFactory } from './domain/ocpp/message.factory';
 import { config } from '../config';
 
-const chargePoint = new ChargePoint('Simulator Inc.', 'Sim-X1');
+const chargePoint = new ChargePoint();
 const ocppMessageFactory = MessageFactory.create(chargePoint.version);
 
-const webSocketClient = new WebSocketClient(chargePoint.id, config.messaging.websocket.url, `ocpp${chargePoint.version}`);
+const wsClient = new WebSocketClient(chargePoint.id, config.messaging.websocket.url, `ocpp${chargePoint.version}`);
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// Note: Simplification
+const scenarios = [
+	{
+		description: 'Connect -> Ping -> OCPP.Heartbeat -> Close',
+		action: async () => {
+			await wsClient.connect();
+			await wsClient.ping();
+			await sleep(1000);
+
+			chargePoint.sendHeartbeat(wsClient, ocppMessageFactory);
+			await sleep(1000);
+
+			await wsClient.close();
+		}
+	}
+];
 
 async function startSimulation() {
 	try {
-		await webSocketClient.connect();
-		chargePoint.sendHeartbeat(webSocketClient, ocppMessageFactory);
-
-		await webSocketClient.close();
+		for (const scenario of scenarios) {
+			console.log(`\n--- Running Scenario: ${scenario.description} ---`);
+			await scenario.action();
+			console.log(`\n--- Scenario Finished: ${scenario.description} ---`);
+		}
 	} catch (error) {
 		console.error(`[${chargePoint.name}] Failed to connect or run simulation:`, error);
 	}
+	console.log('\n--- All Scenarios Finished ---');
+	process.exit(0);
 }
 
 startSimulation();
